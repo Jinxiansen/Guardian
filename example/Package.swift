cd84027837f53b69eb5756c783b10079ad930364
晋先森 <hi@jinxiansen.com>
2018-06-14 00:45:52 +0800

    add example

---
+++ example/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "example",
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+
+        .package(url: "https://github.com/Jinxiansen/Guardian.git", from: "3.0.0"),
+        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2")
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["Guardian", "FluentSQLite", "Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
+    ]
+)
+
